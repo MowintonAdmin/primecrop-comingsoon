0bb18d5853534dbd30f8c437a708c8681c2a4204
Jasmine Toh <jasmine@Jasmines-MacBook-Pro.local>
2026-08-01 00:15:01 +0800

    Add logo, favicon, PHP mail handler for signups

---
+++ subscribe.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['email'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header('Location: index.html?signup=error');
+        exit;
+    }
+    $to = 'jt@mowinton.com';
+    $subject = 'New PrimeCrop Signup';
+    $message = "New subscriber email: " . $email . "\n\nDate: " . date('Y-m-d H:i:s');
+    $headers = 'From: hello@primecrop.my' . "\r\n" . 'Reply-To: ' . $email;
+    mail($to, $subject, $message, $headers);
+    header('Location: index.html?signup=success');
+    exit;
+}
+header('Location: index.html');
